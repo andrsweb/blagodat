a82cf810f16b2f11e95a5318b6772bd11c02e0a9
andrsweb <d3vv.proo@gmail.com>
2025-11-08 21:04:40 +0700

    feat: create about section

--- src/js/common/common.js
+++ src/js/common/common.js
@@ -1,3 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
 	'use strict'
+
+	scrollDown( 'about')
 })
+
+const scrollDown = (targetId) => {
+	const button = document.querySelector('.scroll-down')
+	const target = document.getElementById(targetId)
+
+	if (button && target) {
+		button.addEventListener('click', () => {
+			target.scrollIntoView({ behavior: 'smooth' })
+		})
+	}
+}
