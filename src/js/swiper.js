e26b8c0b0a42b684c8394ad7a75e3fd2af7aee83
andrsweb <d3vv.proo@gmail.com>
2025-11-09 18:06:33 +0700

    feat: create slides section

---
+++ src/js/swiper.js
@@ -0,0 +1,52 @@
+import Swiper from 'swiper';
+import { Navigation, Scrollbar } from "swiper/modules";
+
+document.addEventListener('DOMContentLoaded', () => {
+    'use strict';
+
+    initSwiper('.slider');
+});
+
+const commonSwiperOptions = {
+    grabCursor: true,
+    speed: 1000,
+    loop: true,
+    modules: [Navigation, Scrollbar],
+};
+
+function initSwiper(selector) {
+    const container = document.querySelector(selector);
+    if (!container) return;
+
+    new Swiper(container, {
+        ...commonSwiperOptions,
+        modules: [...commonSwiperOptions.modules],
+        navigation: {
+            nextEl: container.querySelector('.swiper-next'),
+            prevEl: container.querySelector('.swiper-prev'),
+        },
+
+        scrollbar: {
+            el: container.querySelector('.swiper-scrollbar'),
+            draggable: true,
+        },
+
+        breakpoints: {
+            320: {
+                slidesPerView: 1,
+                spaceBetween: 10,
+            },
+            480: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+            },
+            992: {
+                slidesPerView: 3,
+                spaceBetween: 30,
+            },
+            1200: {
+                slidesPerView: 4
+            }
+        }
+    });
+}
